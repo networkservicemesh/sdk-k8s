9eeb828a3001458cbbd2c9dfc08f5eca89a9421c
Tigran Manasyan <manasyan_tigran@mail.ru>
2020-09-04 15:42:04 +0700

    Package comment added
    Signed-off-by: Tigran Manasyan <manasyan_tigran@mail.ru>

--- pkg/utils/kubeutils.go
+++ pkg/utils/kubeutils.go
@@ -14,6 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package utils contains different util functions for kubernetes interfaces and structures
 package utils
 
 import (
