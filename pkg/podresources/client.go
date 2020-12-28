bdbd2f19bc13f201b13e053337aee6ef80ba8228
Vladimir Popov <vladimir.popov@xored.com>
2020-12-28 20:38:25 +0700

    Add !windows tag
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

--- pkg/podresources/client.go
+++ pkg/podresources/client.go
@@ -14,6 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//+build !windows
+
 // Package podresources provides a client for k8s podresources API
 package podresources
 
