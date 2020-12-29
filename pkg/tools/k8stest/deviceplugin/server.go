b04374a863b8324b1dade22a7ea32fa37a059781
Vladimir Popov <vladimir.popov@xored.com>
2020-12-29 10:28:18 +0700

    Add !windows tag for files using unix sockets
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

--- pkg/tools/k8stest/deviceplugin/server.go
+++ pkg/tools/k8stest/deviceplugin/server.go
@@ -14,6 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//+build !windows
+
 // Package deviceplugin provides device plugin GRPC API stubs for testing
 package deviceplugin
 
--- pkg/tools/socketpath/socket_path.go
+++ pkg/tools/socketpath/socket_path.go
@@ -14,6 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//+build !windows
+
 // Package socketpath provides unix socket path helping tools
 package socketpath
 
